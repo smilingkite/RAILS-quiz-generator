b7fc5f9ed11badb2a144c569d0a10276d3f449db
smilingkite <mail@katinkahesselink.net>
2017-11-30 13:38:37 +0100

    Create show and new quiz pages

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,29 @@
 class QuizzesController < ApplicationController
   def index
-    @Quizzes = Quiz.all
+    @quizzes = Quiz.all
+  end
+
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+# renders a form for creating a new resource - HTTP GET.
+# creates new unsaved record and renders the form.
+  def new
+    @quiz = Quiz.new
+  end
+
+# HTTP POST takes record created in 'new' passes to create action,
+# attempts to save to database
+  def create
+    quiz_params = params.require(:quiz).permit(:name)
+
+    @quiz = Quiz.new(quiz_params)
+
+    if @quiz.save
+       redirect_to @quiz
+    else
+       render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root "quizzes#index"
 
   get "quizzes" => "quizzes#index"
+  get 'quizzes/new' => "quizzes#new", as: :new_quiz
+  get "quizzes/:id" => "quizzes#show", as: :quiz
 
+  post "quizzes" => "quizzes#create"
 end
