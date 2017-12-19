3638ba3b614a57f2d3ad9731a3359ac8ce13df51
smilingkite <mail@katinkahesselink.net>
2017-12-19 11:01:33 +0100

    add validations to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+  has_many :answers
+
+  validates :vraag, presence: true
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,10 @@
 class Quiz < ApplicationRecord
+
+  has_many :questions
+
   validates :name, presence: true
   validates :name, uniqueness: true
-  has_many :questions
+
   scope :order_by_name, -> { order(:name) }
 
 end
